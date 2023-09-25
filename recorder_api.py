6a1596db7b0a350b97e255fa0947a0e2e09a3a76
fedebecat <federico.becattini@unifi.it>
2023-09-25 12:00:23 +0200

    better synch but rgb thread needs fixing

--- recorder_api.py
+++ recorder_api.py
@@ -25,22 +25,24 @@ class Recorder:
     def __init__(self, output_dir):
         self.output_dir = output_dir
         # Thread events
-        self.thread_event = Event() # thread event for stopping RGB camera
-        self.event_camera_event = Event() # thread event for Event camera
-        self.event_camera_has_stopped = Event()
+        self.stop_recording_trigger = Event() # thread event for stopping RGB camera
+        self.ev_start_trigger = Event() # thread event for Event camera
+        self.event_camera_has_stopped_trigger = Event()
 
         # Output folder
         self.recording_name = "recording_" + datetime.now().strftime('%H:%M:%S.%f')
-        self.log_folder = os.path.join(output_dir, self.recording_name)
+        self.log_folder = os.path.join(self.output_dir, self.recording_name)
         os.makedirs(self.log_folder, exist_ok=True)
         os.makedirs(self.log_folder + '/frames', exist_ok=True)
+        self.event_log_path = self.log_folder + "/event.raw"
+        print(f"Recording to {self.log_folder}")
 
         # RGB camera thread
-        self.cam_record = Process(target=self.record_video_till_stop)
+        self.cam_record = Process(target=self.record_rgb_synch)
+        self.cam_record.start()
 
         # Init event camera
         self.device = initiate_device("")
-        self.event_log_path = self.log_folder + "/event.raw"
         self.evt_start_timestamp = None
 
         # Event camera thread
@@ -72,19 +74,92 @@ class Recorder:
                 evt_start_timestamp = time.time()
                 print(f"evt_start_timestamp: {evt_start_timestamp}")
 
-            #print(self.thread_event.is_set())
-            if self.thread_event.is_set():
+            if self.stop_recording_trigger.is_set():
                 if close_time is None:
                     close_time = self.get_closing_time()
                     print(f'stopping event camera at {close_time}...')
 
                 # Stop the recording
-                #print(datetime.now(), close_time)
                 if datetime.now() > close_time:
                     self.device.get_i_events_stream().stop_log_raw_data()
-                    self.event_camera_has_stopped.set()
+                    self.event_camera_has_stopped_trigger.set()
                     print('done')
                     break
+
+    
+    def record_rgb_synch(self):
+        #start video capture
+        # vid_w = 320
+        # vid_h = 240
+        cap = cv2.VideoCapture(0)
+        cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+        # cap.set(cv2.CAP_PROP_FRAME_WIDTH, vid_w)
+        # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, vid_h)
+
+        # set CAP_PROP_FORMAT
+        # cap.set(cv2.CAP_PROP_FORMAT, 0)
+
+        # set framerate
+        FPS = 30.0
+        cap.set(cv2.CAP_PROP_FPS, FPS)
+        freq_frames = 1/FPS
+        frame_buffer = []
+
+        # be sure that the internal camera buffer is empty
+        for _ in range(5):
+            cap.grab()
+
+        close_timestamp = None
+        is_recording = False
+
+        # grab frames continously but store in buffer only at the desired frequency
+        while True:
+            grabbed = cap.grab()
+
+            if self.ev_start_trigger.is_set() and not is_recording:
+                print('starting rgb camera...')
+                is_recording = True
+                # store first frame
+                _, frame = cap.retrieve()
+                start_timestamp = datetime.now()
+                last_frame_timestamp = start_timestamp
+                frame_buffer.append({'frame': frame, 'timestamp': last_frame_timestamp})
+
+            if is_recording:
+                # store frame at desired frequency
+                if (datetime.now() - last_frame_timestamp).total_seconds() > freq_frames:
+                    _, frame = cap.retrieve()
+                    last_frame_timestamp = datetime.now()
+                    frame_buffer.append({'frame': frame, 'timestamp': last_frame_timestamp})
+            
+                if self.stop_recording_trigger.is_set() and is_recording:
+                    if close_timestamp is None:
+                        close_timestamp = self.get_closing_time()
+                        print(f'stopping rgb camera at {close_timestamp}...')
+                    if datetime.now() > close_timestamp:
+                        is_recording = False
+
+                        duration = close_timestamp - start_timestamp
+                        print(f"duration: {duration.total_seconds()}")
+                        fps = len(frame_buffer)/(duration.total_seconds())
+                        print(f"frames: {len(frame_buffer)}")
+                        print(f"actual fps: {fps} ---- desired fps: {FPS}")
+                        print(f"len(frame_buffer): {len(frame_buffer)}")
+                        print(f"duration: {duration}")
+                        # The following line initiates the video object and video file named 'video.avi' 
+                        # of width and height declared at the beginning.
+                        print("saving video frames")
+                        # The loop goes through the array of images and writes each image to the video file
+                        for i in range(len(frame_buffer)):
+                            # save video as frames in the frames folder. Add timestamp to filename
+                            cur_timestamp = frame_buffer[i]['timestamp']
+                            if cur_timestamp <= close_timestamp:
+                                cv2.imwrite(f"{self.log_folder}/frames/frame_{i}_{cur_timestamp.strftime('%H:%M:%S.%f')}.jpg",
+                                            frame_buffer[i]['frame'])
+            
+                        frame_buffer = []
+                        print("Done")
+
 
     def record_video_till_stop(self):
         #start video capture
@@ -93,7 +168,6 @@ class Recorder:
         capture = VideoCaptureAsyncWithTimestamp(src=0, width=vid_w, height=vid_h)
         capture.start()
 
-        frames = 0
         #Create array to hold frames from capture
         images = []
         # Capture for duration defined by variable 'duration'
@@ -104,19 +178,16 @@ class Recorder:
         start_time = None
         close_time = None
         while True:
-            # print('iter')
             time_elapsed = datetime.now() - prev
             ret, new_frame, timestamp = capture.read()
 
-            if time_elapsed > fps_delay and self.event_camera_event.is_set():
+            if time_elapsed > fps_delay and self.ev_start_trigger.is_set():
                 if start_time is None:
                     start_time = datetime.now()
                     print(f"start_time: {start_time}")
-                # print(time_elapsed - fps_delay)
                 prev = timestamp
-                frames += 1
                 images.append({'frame': new_frame, 'timestamp': prev})
-            if self.thread_event.is_set():
+            if self.stop_recording_trigger.is_set():
                 if close_time is None:
                     close_time = self.get_closing_time()
                     print(f'stopping rgb camera at {close_time}...')
@@ -124,7 +195,6 @@ class Recorder:
                     break
         duration = datetime.now() - start_time
         capture.stop()
-        cv2.destroyAllWindows()
         # The fps variable which counts the number of frames and divides it by 
         # the duration gives the frames per second which is used to record the video later.
         print(f"duration: {duration.seconds + duration.microseconds/(1000*1000)}")
@@ -148,17 +218,22 @@ class Recorder:
         print("Done")
 
     def start_recording_rgb_and_event(self):
-        self.cam_record.start()
-        self.event_camera_event.set()
+        #self.cam_record.start()
+        self.ev_start_trigger.set()
         self.event_record.start()
 
     def stop_recording_rgb_and_event(self):
         close_time = datetime.now() + timedelta(seconds=2)
         close_time -= timedelta(microseconds=close_time.microsecond)
         print(f"close_time: {close_time}")
-        self.thread_event.set()
+        self.stop_recording_trigger.set()
+        # reset trigger
+        self.ev_start_trigger.clear()
+
         self.cam_record.join()
         self.cam_record.close()
+        self.event_record.join()
+        self.event_record.close()
 
     def get_closing_time(self):
         close_time = datetime.now() + timedelta(seconds=2)
@@ -167,15 +242,57 @@ class Recorder:
         return close_time
 
 
+def sanity_check(recordings_folder):
+    # Read RGB frames and extract timestamps from filenames to check the framerate is ok
+    rgb_frames = []
+    for filename in os.listdir(recordings_folder + '/frames'):
+        if filename.endswith(".jpg"):
+            rgb_frames.append({'frame': cv2.imread(os.path.join(recordings_folder + '/frames', filename)),
+                               'timestamp': filename.split('_')[2].replace('.jpg','')})
+    rgb_frames.sort(key=lambda x: x['timestamp'])
+    print(f"len(rgb_frames): {len(rgb_frames)}")
+    timestamps = [x['timestamp'] for x in rgb_frames]
+    # convert timestaps to datetime objects parsing strings like 09:40:48.042800
+    timestamps = [datetime.strptime(str(x), '%H:%M:%S.%f') for x in timestamps]
+
+    print(timestamps)
+    normalized_timestamps = [x - timestamps[0] for x in timestamps]
+    diff_timestamps = [timestamps[i+1] - timestamps[i] for i in range(len(timestamps)-1)]
+    # mean diff
+    print('checking rgb framerate...')
+    print(f"mean diff: {sum(diff_timestamps)/len(diff_timestamps)}")
+    print(f'FPS: {1/(sum(diff_timestamps)/len(diff_timestamps))}')
+    print(f'duration: {timestamps[-1] - timestamps[0]}')
+
+
 rec = Recorder('recordings')
+
+time.sleep(3)
+
 rec.start_recording_rgb_and_event()
 
 # wait 5 seconds
 start_time = time.time()
 
-while time.time() - start_time < 5:
+while time.time() - start_time < 6:
     #print('waiting...')
     pass
 print('done waiting')
 
 rec.stop_recording_rgb_and_event()
+
+# time.sleep(3)
+
+# rec.start_recording_rgb_and_event()
+
+# # wait 5 seconds
+# start_time = time.time()
+
+# while time.time() - start_time < 6:
+#     #print('waiting...')
+#     pass
+# print('done waiting')
+
+# rec.stop_recording_rgb_and_event()
+
+#sanity_check(rec.log_folder)
